2cab813226611504c107e6d17a049f87d4005de3
a9ee8aa9ee8a <qa124143266@gmail.com>
2021-04-28 20:43:04 +0800

    20210428 Modify by jack: 1.邏輯運算 2.建立函數

--- day03/bmi多筆計算.py
+++ day03/bmi多筆計算.py
@@ -1,4 +1,5 @@
-import  math
+import math
+
 '''
     有三組資料:
     170, 50;
@@ -6,16 +7,27 @@ import  math
     160, 60;
     計算bmi
 '''
-def PrintBMI(h, w):
-    bmi = w /math.pow((h/100), 2)
-    if bmi > 23:
-        result = "過重"
-    elif bmi < 18:
-        result = "過輕"
-    else:
+
+
+def PrintBmi(h, w):
+    bmi = w / math.pow((h / 100), 2)
+    # result = "過重" if bmi > 23 else "過重" if bmi <= 18 else "正常"
+
+    # if bmi > 23:
+    #     result = "過重"
+    # elif bmi < 18:
+    #     result = "過輕"
+    # else:
+    #     result = "正常"   #ctrl + / 快速註解
+    result = "過輕"
+    if 18 < bmi <= 23:
         result = "正常"
+    elif bmi > 23:
+        result = "過重"
+
     print(" h=%.1f w=%.1f bmi=%.2f  %s" % (h, w, bmi, result))
 
-PrintBMI(170, 50)
-PrintBMI(180, 70)
-PrintBMI(160, 60)
+
+PrintBmi(170, 50)
+PrintBmi(180, 70)
+PrintBmi(160, 60)
